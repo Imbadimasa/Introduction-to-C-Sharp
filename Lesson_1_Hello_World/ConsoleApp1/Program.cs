e6bb948c3c8bbfc7d474127d3cf1ebb364835eb8
Imbadimasa <chalganovdima@gmail.com>
2021-08-29 14:57:25 -0400

    Final version of Homework 1. Includes .gitignore.

--- Lesson_1_Hello_World/ConsoleApp1/Program.cs
+++ Lesson_1_Hello_World/ConsoleApp1/Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Введите ваше имя.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня [{DateTime.Now}]");
+            Console.ReadLine();
         }
     }
 }
