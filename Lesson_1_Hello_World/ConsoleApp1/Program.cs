894cdde85212a8ed150588280501539ef82b5692
Imbadimasa <chalganovdima@gmail.com>
2021-08-30 22:25:41 -0400

    Added Comments to first task

--- Lesson_1_Hello_World/ConsoleApp1/Program.cs
+++ Lesson_1_Hello_World/ConsoleApp1/Program.cs
@@ -6,12 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //User inputs Lowest Temperature
             Console.WriteLine("Please enter lowest temperature today.");
             double lowestTemperature = Convert.ToDouble (Console.ReadLine());
 
+            //User inputs Highest Temperature
             Console.WriteLine("Please enter highest temperature today.");
             double highestTemperature = Convert.ToDouble(Console.ReadLine());
 
+            //Calculating Average
             double averageTemperature = (lowestTemperature + highestTemperature) / 2;
             Console.WriteLine($"Average temperature for today is {averageTemperature}");
 
