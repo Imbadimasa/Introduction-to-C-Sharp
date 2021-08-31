61798fe29cddf79044f1b150cf7706fbaad71ef9
Imbadimasa <chalganovdima@gmail.com>
2021-08-30 22:21:30 -0400

    First Task of Homework 2. Average Temperature.

--- Lesson_1_Hello_World/ConsoleApp1/Program.cs
+++ Lesson_1_Hello_World/ConsoleApp1/Program.cs
@@ -6,26 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int knowledges = 0b011111; // знания студента
+            Console.WriteLine("Please enter lowest temperature today.");
+            double lowestTemperature = Convert.ToDouble (Console.ReadLine());
 
-            // Маски профессий:
-            int backenderMask = 0b010111;
-            int frontenderMask = 0b011010;
-            int designerMask = 0b110000;
+            Console.WriteLine("Please enter highest temperature today.");
+            double highestTemperature = Convert.ToDouble(Console.ReadLine());
 
-            // Те навыки из каждой профессии, которые присутствуют у студента:
-            int backenderKnowledges = knowledges & backenderMask;
-            int frontenderKnowledges = knowledges & frontenderMask;
-            int designerKnowledges = knowledges & designerMask;
-
-            // Если навыки полностью совпали с маской, мы получим True, иначе False
-            Console.WriteLine(backenderKnowledges == backenderMask);
-            Console.WriteLine(frontenderKnowledges == frontenderMask);
-            Console.WriteLine(designerKnowledges == designerMask);
+            double averageTemperature = (lowestTemperature + highestTemperature) / 2;
+            Console.WriteLine($"Average temperature for today is {averageTemperature}");
 
             Console.ReadLine();
-
-
         }
     }
 }
