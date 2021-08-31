f5d0edadb673d38c442b0f635fcb84a7f553e723
Imbadimasa <chalganovdima@gmail.com>
2021-08-30 21:56:30 -0400

    Testing the bit mask Homework 2

--- Lesson_1_Hello_World/ConsoleApp1/Program.cs
+++ Lesson_1_Hello_World/ConsoleApp1/Program.cs
@@ -6,10 +6,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите ваше имя.");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Привет, {name}, сегодня [{DateTime.Now}]");
+            int knowledges = 0b011111; // знания студента
+
+            // Маски профессий:
+            int backenderMask = 0b010111;
+            int frontenderMask = 0b011010;
+            int designerMask = 0b110000;
+
+            // Те навыки из каждой профессии, которые присутствуют у студента:
+            int backenderKnowledges = knowledges & backenderMask;
+            int frontenderKnowledges = knowledges & frontenderMask;
+            int designerKnowledges = knowledges & designerMask;
+
+            // Если навыки полностью совпали с маской, мы получим True, иначе False
+            Console.WriteLine(backenderKnowledges == backenderMask);
+            Console.WriteLine(frontenderKnowledges == frontenderMask);
+            Console.WriteLine(designerKnowledges == designerMask);
+
             Console.ReadLine();
+
+
         }
     }
 }
