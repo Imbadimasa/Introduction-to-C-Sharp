3919d601b37a34d95e937b26fc3058b9e8bb3024
Imbadimasa <chalganovdima@gmail.com>
2021-09-05 13:38:10 -0400

    Tasks 3.1 diagonal arrays. It is a little bit ambiguous, so I've created 2 versions.

--- Homework_3_Arrays/Program.cs
+++ Homework_3_Arrays/Program.cs
@@ -6,7 +6,58 @@ namespace Homework_3_Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!!");
+            int value = 10;
+            int xLocation = 0;
+            int yLocation = 0;
+            int[,] matrix = new int[10, 10];
+
+            Console.WriteLine("Which Version do you want to see: 1 or 2?");
+            int version = Convert.ToInt32(Console.ReadLine());
+
+
+            if (version == 1)
+            {
+                for (int i = 1; i < matrix.GetLength(0); i++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = value;
+                        //Console.CursorLeft = xLocation;
+                        //Console.CursorLeft = yLocation;
+                        Console.Write(matrix[i, j]);
+                        Console.Write(" ");
+                        value++;
+                        xLocation++;
+                        yLocation++;
+
+                    }
+
+                }
+
+            }
+            else if (version == 2)
+            {
+                for (int i = 1; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = value;
+                        Console.CursorLeft = xLocation;
+                        Console.CursorLeft = yLocation;
+                        Console.WriteLine(matrix[i, j]);
+                        value++;
+                        xLocation++;
+                        yLocation++;
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("This version does not exist!");
+            }
+            Console.ReadLine();
         }
     }
 }
